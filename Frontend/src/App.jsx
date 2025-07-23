dc4714f184015ddb587c035ab2f07e733d1a2454
Shumail Khan <shumailkhan7878@gmail.com>
2025-07-23 17:00:47 +0500

    Added Routes and Created Header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header.jsx"
 function App() {
   return (
       <div className="">
         <Header />
         Welcome to Tree Project
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}></Route>
+          <Route path="/about-us" element={<h1>About Us</h1>}></Route>
+          <Route path="/services" element={<h1>Services</h1>}></Route>
+          <Route path="/service-areas" element={<h1>Service Areas</h1>}></Route>
+          <Route path="/photo-gallery" element={<h1>Photo Gallery</h1>}></Route>
+          <Route path="/contact-us" element={<h1>Contact Us</h1>}></Route>
+          <Route path="/blog" element={<h1>Blogs</h1>}></Route>
+        </Routes>
       </div>
   )
 }
