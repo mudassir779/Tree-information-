b38167ebf5e9d826b35df76575cb16e99978bf32
Shumail Khan <shumailkhan7878@gmail.com>
2025-07-29 18:17:57 +0500

    Logo and Footer Designed, and Minor Bugs Removed

---
+++ Backend/Model/Request.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const RequestSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
